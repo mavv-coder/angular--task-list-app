996740472b2d66f98bdb3b361d8122f68d1ac299
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-07 21:08:19 +0200

    adding task working

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  taskName: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private taskService: TaskService) {
+    this.taskName = '';
   }
 
+  ngOnInit(): void {}
+
+  addTask(e): void {
+    e.preventDefault();
+    this.taskService.addNewTask(this.taskName);
+    this.taskName = '';
+  }
+
+  updateTask(value: string): void {
+    this.taskName = value;
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -18,4 +18,19 @@ export class TaskService {
   getTaskList(): Observable<Task[]> {
     return of(this.taskList);
   }
+
+  addNewTask(name: string): void {
+    const newTask = { name, id: this.generateId() };
+    this.taskList.push(newTask);
+  }
+
+  generateId(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (
+      c
+    ) {
+      var r = (Math.random() * 16) | 0,
+        v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
 }
