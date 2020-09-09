5ab578745d6233d799af0247699beb9c1d220fd9
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-09 09:50:23 +0200

    problems of mutability

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { getTaskList } from '../api/mock-data';
 import { of, Observable } from 'rxjs';
 import { Task } from '../model/task';
+import { ThrowStmt } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
@@ -21,33 +22,48 @@ export class TaskService {
     return of(this.taskList);
   }
 
+  getCurrentTask(): Observable<Task> {
+    return of(this.currentTask);
+  }
+
+  // Mira aquí, Carlos !!!
   addNewTask(name: string): void {
-    const newTask = { name, id: this.generateId() };
-    this.taskList.push(newTask);
+    // const newTask = {  id: this.generateId(), name };
+    // this.taskList.push(newTask);
+    const newTask = { id: this.generateId(), name };
+    const newTaskList = [...this.taskList, newTask];
+    this.taskList = newTaskList;
+    console.log(this.taskList);
   }
 
+  // Mira aquí, Carlos !!!
   deleteTask(id: string): void {
-    const index = this.taskList.findIndex((x) => x.id === id);
-    this.taskList.splice(index, 1);
+    // const index = this.taskList.findIndex((x) => x.id === id);
+    // this.taskList.splice(index, 1);
+    const newTaskList = this.taskList.filter((x) => x.id !== id);
+    this.taskList = newTaskList;
+    console.log(this.taskList);
   }
 
+  // Mira aquí, Carlos !!!
   clearCurrentTask() {
-    this.currentTask.name = '';
-    this.currentTask.id = '';
+    // this.currentTask.name = '';
+    // this.currentTask.id = '';
+    this.currentTask = { name: '', id: '' };
+    console.log(this.currentTask);
   }
 
+  // Mira aquí, Carlos !!!
   selectEditTask(task: Task): void {
-    this.currentTask.name = task.name;
-    this.currentTask.id = task.id;
+    // this.currentTask.name = task.name;
+    // this.currentTask.id = task.id;
+    this.currentTask = task;
+    console.log(this.currentTask);
   }
 
   editTask(task: Task): void {
     const index = this.taskList.findIndex((x) => x.id === this.currentTask.id);
     this.taskList.splice(index, 1, task);
-  }
-
-  getCurrentTask(): Observable<Task> {
-    return of(this.currentTask);
   }
 
   generateId(): string {
